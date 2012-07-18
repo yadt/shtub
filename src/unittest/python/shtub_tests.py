b9727253a7268356785710248def8dfeb483e6a4
Michael Gruber <aelgru@gmail.com>
2012-07-18 16:34:02 +0200

    using convention mock_<name>

--- src/unittest/python/shtub_tests.py
+++ src/unittest/python/shtub_tests.py
@@ -16,7 +16,7 @@
 
 import unittest
 
-from mock import call, patch, Mock
+from mock import Mock, call, patch
 
 from StringIO import StringIO
 
@@ -29,32 +29,32 @@ from shtub.expectation import Expectation
 class IntegrationtestsTests (unittest.TestCase):
     @patch('json.loads')
     @patch('__builtin__.open')
-    def test_should_deserialize_stub_executions (self, open_mock, json_mock):
-        fake_file = self.return_file_when_calling(open_mock)
+    def test_should_deserialize_stub_executions (self, mock_open, mock_json):
+        fake_file = self.return_file_when_calling(mock_open)
         executions = [Execution('command', ['-arg1', '-arg2', '-arg3'], 'stdin')]
         json_string = "[{'command': 'command', 'arguments': ['-arg1', '-arg2', '-arg3'], 'stdin': 'stdin'}]"
         fake_file.read.return_value = json_string
-        json_mock.return_value = [{'command'   : 'command',
+        mock_json.return_value = [{'command'   : 'command',
                                    'arguments' : ['-arg1', '-arg2', '-arg3'],
                                    'stdin'     : 'stdin'}]
                 
         actual_executions = deserialize_stub_executions('executions.json')
         
-        self.assertEquals(call('executions.json', 'r'), open_mock.call_args)
+        self.assertEquals(call('executions.json', 'r'), mock_open.call_args)
         self.assertEquals(call(), fake_file.read.call_args)
-        self.assertEquals(call(json_string), json_mock.call_args)
+        self.assertEquals(call(json_string), mock_json.call_args)
         
         expected_executions = [Execution('command', ['-arg1', '-arg2', '-arg3'], 'stdin')]
         self.assertEquals(expected_executions, actual_executions)
 
     @patch('json.loads')
     @patch('__builtin__.open')
-    def test_should_deserialize_expectations (self, open_mock, json_mock):
-        fake_file = self.return_file_when_calling(open_mock)
+    def test_should_deserialize_expectations (self, mock_open, mock_json):
+        fake_file = self.return_file_when_calling(mock_open)
         executions = [Expectation('command', ['-arg1', '-arg2', '-arg3'], 'stdin')]
         json_string = "[{'command': 'command', 'arguments': ['-arg1', '-arg2', '-arg3'], 'stdin': 'stdin', 'current_answer': 0, 'answers': []} ]"
         fake_file.read.return_value = json_string
-        json_mock.return_value = [{'command'        : 'command',
+        mock_json.return_value = [{'command'        : 'command',
                                    'arguments'      : ['-arg1', '-arg2', '-arg3'],
                                    'stdin'          : 'stdin',
                                    'current_answer' : 0,
@@ -65,9 +65,9 @@ class IntegrationtestsTests (unittest.TestCase):
                 
         actual_expectations = deserialize_expectations('executions.json')
         
-        self.assertEquals(call('executions.json', 'r'), open_mock.call_args)
+        self.assertEquals(call('executions.json', 'r'), mock_open.call_args)
         self.assertEquals(call(), fake_file.read.call_args)
-        self.assertEquals(call(json_string), json_mock.call_args)
+        self.assertEquals(call(json_string), mock_json.call_args)
         
         expected_expectations = [Expectation('command', ['-arg1', '-arg2', '-arg3'], 'stdin', [Answer('stdout', 'stderr', 15)], 0)]
         
@@ -75,9 +75,9 @@ class IntegrationtestsTests (unittest.TestCase):
 
     @patch('json.dumps')
     @patch('__builtin__.open')
-    def test_should_serialize_stub_executions (self, open_mock, json_mock):
-        fake_file = self.return_file_when_calling(open_mock)
-        json_mock.return_value = '[{"some": "json"}]'
+    def test_should_serialize_stub_executions (self, mock_open, mock_json):
+        fake_file = self.return_file_when_calling(mock_open)
+        mock_json.return_value = '[{"some": "json"}]'
         executions = [Execution('command', ['-arg1', '-arg2', '-arg3'], 'stdin')]
 
         serialize_stub_executions('executions.json', executions)
@@ -86,17 +86,17 @@ class IntegrationtestsTests (unittest.TestCase):
                                'arguments' : ['-arg1', '-arg2', '-arg3'], 
                                'stdin'     : 'stdin'}
         
-        self.assertEquals(call([expected_dictionary], sort_keys=True, indent=4), json_mock.call_args)
-        self.assertEquals(call('executions.json', 'w'), open_mock.call_args)
+        self.assertEquals(call([expected_dictionary], sort_keys=True, indent=4), mock_json.call_args)
+        self.assertEquals(call('executions.json', 'w'), mock_open.call_args)
         self.assertEquals(call('[{"some": "json"}]'), fake_file.write.call_args)
 
-    def return_file_when_calling (self, open_mock, content=None):
+    def return_file_when_calling (self, mock_open, content=None):
         file_handle = Mock()
         
-        open_mock.return_value.__enter__ = Mock(return_value=file_handle)
-        open_mock.return_value.__exit__ = Mock()
+        mock_open.return_value.__enter__ = Mock(return_value=file_handle)
+        mock_open.return_value.__exit__ = Mock()
         
         if content is not None:
-            open_mock.return_value.read.return_value = content
+            mock_open.return_value.read.return_value = content
         
         return file_handle
--- src/unittest/python/verifier_tests.py
+++ src/unittest/python/verifier_tests.py
@@ -33,65 +33,65 @@ class VerfierTest (unittest.TestCase):
         self.assertEquals([], actual_verifier.recorded_calls)
     
     @patch('shtub.verifier.deserialize_stub_executions')
-    def test_should_deserialize_recorded_calls_and_return_verifier_itself_when_entering_with_statement (self, deserialize_mock):
+    def test_should_deserialize_recorded_calls_and_return_verifier_itself_when_entering_with_statement (self, mock_deserialize):
         verifier = Verifier('/hello/world')
                 
         with verifier as veri:
             self.assertEquals(veri, verifier)
             
-        self.assertEquals(call('/hello/world/test-execution/recorded-calls'), deserialize_mock.call_args)
+        self.assertEquals(call('/hello/world/test-execution/recorded-calls'), mock_deserialize.call_args)
     
     @patch('shtub.verifier.deserialize_stub_executions')
-    def test_should_raise_exception_when_no_executed_calls_recorded (self, deserialize_mock):
+    def test_should_raise_exception_when_no_executed_calls_recorded (self, mock_deserialize):
         verifier = Verifier('/hello/world')
         
-        deserialize_mock.return_value = None
+        mock_deserialize.return_value = None
         
         with verifier as veri:
             self.assertRaises(AssertionError, veri.verify, 'any_stub', ['any_arguments'], 'any_stdin')
 
-        self.assertEquals(call('/hello/world/test-execution/recorded-calls'), deserialize_mock.call_args)
+        self.assertEquals(call('/hello/world/test-execution/recorded-calls'), mock_deserialize.call_args)
 
     @patch('shtub.verifier.deserialize_stub_executions')
-    def test_should_raise_exception_when_recorded_call_does_not_fit_expectation (self, deserialize_mock):
+    def test_should_raise_exception_when_recorded_call_does_not_fit_expectation (self, mock_deserialize):
         verifier = Verifier('/hello/world')
         
         stub_execution = Execution('any_command', ['any_arg1', 'any_arg2'], 'any_stdin')
         
-        deserialize_mock.return_value = [stub_execution]
+        mock_deserialize.return_value = [stub_execution]
         
         with verifier as veri:
             self.assertRaises(AssertionError, veri.verify, 'other_command', ['any_arg1', 'any_arg2'], 'any_stdin')
 
-        self.assertEquals(call('/hello/world/test-execution/recorded-calls'), deserialize_mock.call_args)
+        self.assertEquals(call('/hello/world/test-execution/recorded-calls'), mock_deserialize.call_args)
 
     @patch('shtub.verifier.deserialize_stub_executions')
-    def test_should_raise_exception_when_second_recorded_call_does_not_fit_expectation (self, deserialize_mock):
+    def test_should_raise_exception_when_second_recorded_call_does_not_fit_expectation (self, mock_deserialize):
         verifier = Verifier('/hello/world')
         
         stub_execution1 = Execution('any_command1', ['1any_arg1', '1any_arg2'], 'any_stdin')
         stub_execution2 = Execution('any_command2', ['2any_arg1', '2any_arg2'], 'any_stdin2')
         
-        deserialize_mock.return_value = [stub_execution1, stub_execution2]
+        mock_deserialize.return_value = [stub_execution1, stub_execution2]
         
         with verifier as veri:
             veri.verify('any_command1', ['1any_arg1', '1any_arg2'], 'any_stdin')
             self.assertRaises(AssertionError, veri.verify, 'other_command', ['2any_arg1', '2any_arg2'], 'any_stdin2')
 
-        self.assertEquals(call('/hello/world/test-execution/recorded-calls'), deserialize_mock.call_args)
+        self.assertEquals(call('/hello/world/test-execution/recorded-calls'), mock_deserialize.call_args)
 
     @patch('shtub.verifier.deserialize_stub_executions')
-    def test_should_verify_all_recorded_calls_when_all_recorded_calls_fit_expectation (self, deserialize_mock):
+    def test_should_verify_all_recorded_calls_when_all_recorded_calls_fit_expectation (self, mock_deserialize):
         verifier = Verifier('/hello/world')
         
         stub_execution1 = Execution('any_command1', ['1any_arg1', '1any_arg2'], 'any_stdin1')
         stub_execution2 = Execution('any_command2', ['2any_arg1', '2any_arg2'], 'any_stdin2')
         
-        deserialize_mock.return_value = [stub_execution1, stub_execution2]
+        mock_deserialize.return_value = [stub_execution1, stub_execution2]
         
         with verifier as veri:
             veri.verify('any_command1', ['1any_arg1', '1any_arg2'], 'any_stdin1')
             veri.verify('any_command2', ['2any_arg1', '2any_arg2'], 'any_stdin2')
             self.assertEqual(0, len(veri.recorded_calls))
 
-        self.assertEquals(call('/hello/world/test-execution/recorded-calls'), deserialize_mock.call_args)
+        self.assertEquals(call('/hello/world/test-execution/recorded-calls'), mock_deserialize.call_args)
