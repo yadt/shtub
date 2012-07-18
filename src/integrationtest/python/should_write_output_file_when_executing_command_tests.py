247fbbe2b80e922872ea54751d1239c3ff1a8ebd
Michael Gruber <aelgru@gmail.com>
2012-07-18 10:59:30 +0200

    increasing readability

--- src/integrationtest/python/should_write_output_file_when_executing_command_tests.py
+++ src/integrationtest/python/should_write_output_file_when_executing_command_tests.py
@@ -36,13 +36,14 @@ class Test (integrationtest_support.IntegrationTestSupport):
         self.execute_command('command_wrapper')
         self.execute_command('command_wrapper')
 
-        expected_file_content = ("""----------------- ENVIRONMENT -------------------
-PATH=%s
-PYTHONPATH=%s
------------------ STDOUT -------------------
-Hello world!
------------------ STDERR -------------------
-Hello error.""") % (self.create_path(), self.create_python_path())
+        expected_file_content = ('----------------- ENVIRONMENT -------------------\n'
+                                 'PATH=%s\n'
+                                 'PYTHONPATH=%s\n'
+                                 '----------------- STDOUT -------------------\n'
+                                 'Hello world!\n'
+                                 '----------------- STDERR -------------------\n'
+                                 'Hello error.'
+                                ) % (self.create_path(), self.create_python_path())
 
         output_filename_00 = join(self.base_dir, 'test-execution', '00-command-command_wrapper')
         self.assert_file_content(output_filename_00, expected_file_content)
