e89a842b12eff8f573b3daa2d44db3c46d568a60
Michael Gruber <aelgru@gmail.com>
2013-02-11 15:34:20 +0100

    :sparkles: Happy New Year ! :sparkles:

--- src/main/python/shtub/commandstub.py
+++ src/main/python/shtub/commandstub.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 #   shtub - shell command stub
-#   Copyright (C) 2012 Immobilien Scout GmbH
+#   Copyright (C) 2012-2013 Immobilien Scout GmbH
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
