96a74c8c462328fab24ad1425a506a3aace995dc
mriehl <max@riehl.io>
2014-05-26 12:37:51 +0200

    v0.3.6

--- build.py
+++ build.py
@@ -34,7 +34,7 @@ authors = [Author('Alexander Metzner', 'alexander.metzner@gmail.com'),
 license = 'GNU GPL v3'
 summary = 'shtub - shell command stub'
 url = 'https://github.com/yadt/shtub'
-version = '0.3.5'
+version = '0.3.6'
 
 default_task = ['analyze', 'publish']
 
